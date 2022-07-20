2cee17565ec4ff35fd67895a3a4fd2855b2ad790
Alex Kist <akist0707@gmail.com>
2022-07-20 10:50:17 -0500

    Add form info to DOM

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 let myLibrary = [];
 
-function Book(title) {
+function Book(title, author, pageCount, readStatus) {
     this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.readStatus = readStatus;
 }
 
-function addBookToLibrary(title) {
-    let book = new Book(title);
+function addBookToLibrary() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pageCount = document.getElementById("pageCount").value;
+    let readStatus = document.getElementById("readStatus").value;
+    let book = new Book(title, author, pageCount, readStatus);
 
     myLibrary.push(book);
 
-    let newBook = document.createElement("h2");
-    let newBookTitle = document.createTextNode(`${myLibrary[myLibrary.length - 1]}`);
-    newBook.appendChild(newBookTitle);
+    let titleText = document.createElement("h2");
+    titleText.id = `book${myLibrary.length}`;
 
-    document.getElementById("library").appendChild(newBook);
+    document.querySelector(".library").appendChild(titleText);
+
+    let titleTextNode = document.createTextNode(`${myLibrary[myLibrary.length - 1].title}`);
+    document.getElementById(`book${myLibrary.length}`).appendChild(titleTextNode);
+
+    displayForm();
+
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pageCount").value = "";
+    document.getElementById("readStatus").checked = false;
 }
 
 function displayForm() {
-    console.log("test");
-
-    if (document.getElementById("form-container").classList === "form-container-open") {
-        document.getElementById("form-container").classList = "form-container-closed";
+    if (document.getElementById("form-container").classList.value === "form-container-open") {
+        document.getElementById("form-container").classList.value = "form-container-closed";
     }
     else {
-        document.getElementById("form-container").classList = "form-container-open";
+        document.getElementById("form-container").classList.value = "form-container-open";
     }
 }
